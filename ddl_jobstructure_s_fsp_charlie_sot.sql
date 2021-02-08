4e3f3f4c76497ba81652b91892edbc4b1c0c523e
503090274 <gauri.kandarkar@ge.com>
2021-02-08 16:27:19 +0530

    stg_fieldservices

---
+++ ddl_jobstructure_s_fsp_charlie_sot.sql
@@ -0,0 +1,3 @@
+alter table sot_fieldservices.jobstructure_s_fsp_charlie_sot add column billed text;
+
+alter table sot_fieldservices.jobstructure_s_fsp_charlie_sot add column invoice_num varchar(200);
---
+++ ddl_jobstructure_s_fsp_charlie_stg.sql
@@ -0,0 +1,3 @@
+alter table stg_fieldservices.jobstructure_s_fsp_charlie_stg add column billed text;
+
+alter table stg_fieldservices.jobstructure_s_fsp_charlie_stg add column invoice_num varchar(200);
